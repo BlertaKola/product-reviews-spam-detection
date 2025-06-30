df62f0ea0547ea7b1c47610d25431480dacbdc7f
blerta <blertakola4@gmail.com>
2025-06-30 11:15:07 +0200

    rm api key, we dont need it

--- main.py
+++ main.py
@@ -1,14 +1,9 @@
-from fastapi import FastAPI, HTTPException, Depends, Header
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 import torch
 import uvicorn
-from typing import Optional
-from dotenv import load_dotenv
-import os
-load_dotenv()
 
-API_KEY = os.getenv("API_KEY")
 
 MODEL_NAME = "IreNkweke/HamOrSpamModel"
 tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
@@ -25,16 +20,12 @@ class SpamPrediction(BaseModel):
 
 app = FastAPI(title="Spam Detection API", version="1.0")
 
-def verify_api_key(x_api_key: Optional[str] = Header(None)):
-    if x_api_key != API_KEY:
-        raise HTTPException(
-            status_code=401, 
-            detail="Invalid or missing API key. Include 'X-API-Key' header."
-        )
-    return x_api_key
+@app.get("/")
+def read_root():
+    return {"message": "Welcome to the Spam Detection API"}
 
 @app.post("/predict", response_model=SpamPrediction)
-def predict_spam(data: ReviewInput, api_key: str = Depends(verify_api_key)):
+def predict_spam(data: ReviewInput):
     try:
         inputs = tokenizer(data.text, padding=True, truncation=True, return_tensors="pt")
         with torch.no_grad():
